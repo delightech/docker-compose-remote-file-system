7d2573797107baa079f258df14a009f607627738
delightech <hisafumi.kikkawa@gmail.com>
2020-06-04 19:41:48 +0900

    2nd commit

--- sftp.rb
+++ sftp.rb
@@ -5,5 +5,6 @@ option = {
 }
 Net::SFTP.start('localhost', 'sftp-without-key', option) do |sftp|
   #sftp.mkdir!("/remote/test")
+  # put先のディレクトリは/から始まる
   sftp.upload!(File.expand_path("./test.csv"), "/remote/test.csv")
 end
